7bd1b5578f7b380fc6cccf9e85147e79d72efb8b
bs482 <bs482@pvsd.org>
2018-09-13 11:08:55 -0400

    imported code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println( "Enter an Integer:");
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
+
+        if(num%2==0){
+            System.out.println("The integer is even.");
+        }
+        else{
+            System.out.println("The integer is odd.");
+        }
     }
 }
